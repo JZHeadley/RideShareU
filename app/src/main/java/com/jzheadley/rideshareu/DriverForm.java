c96657cdf2e30a73737802cfcb4b27d5dbc9f899
jzheadley <zephyrinkg@gmail.com>
2016-02-27 10:02:35 -0500

    working Views No SQL Yet

--- app/src/main/java/com/jzheadley/rideshareu/DriverForm.java
+++ app/src/main/java/com/jzheadley/rideshareu/DriverForm.java
@@ -2,15 +2,19 @@ package com.jzheadley.rideshareu;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import java.util.Calendar;
 
 public class DriverForm extends AppCompatActivity {
+    private boolean roundTrip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,32 +23,40 @@ public class DriverForm extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-
+        final CheckBox checkBox = (CheckBox) findViewById(R.id.roundTrip_chk);
+        checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkBox.isChecked()) {
+                    ((LinearLayout) findViewById(R.id.returnTripRow)).setVisibility(View.VISIBLE);
+                } else {
+                    ((LinearLayout) findViewById(R.id.returnTripRow)).setVisibility(View.INVISIBLE);
+                }
+                roundTrip = checkBox.isChecked();
+            }
+        });
     }
 
     public void timeSelectionDialog(View view) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
     }
 
     public void datePickerDialog(View view) {
-
-
         final Calendar c = Calendar.getInstance();
         int mYear = c.get(Calendar.YEAR);
         int mMonth = c.get(Calendar.MONTH);
         int mDay = c.get(Calendar.DAY_OF_MONTH);
-
-        DatePickerDialog dpd = new DatePickerDialog(getApplicationContext(), new DatePickerDialog.OnDateSetListener() {
+        DatePickerDialog dpd = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year,
                                   int monthOfYear, int dayOfMonth) {
-                ((EditText) findViewById(R.id.date_et)).setText(dayOfMonth + "-"
-                        + (monthOfYear + 1) + "-" + year);
+                ((EditText) findViewById(R.id.date_et)).setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
 
             }
         }, mYear, mMonth, mDay);
         dpd.show();
-
     }
+
 
 }
